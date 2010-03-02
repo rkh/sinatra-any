e63bf087a548f7dcc5f095c87bb9b21ba1afaa71
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-03-02 23:24:58 +0100

    do the define_method trick

--- lib/sinatra/any.rb
+++ lib/sinatra/any.rb
@@ -5,7 +5,9 @@ module Sinatra
   module Any
 
     def any(path, *args, &blk)
-      passing = lambda { blk.call ; pass }
+      define_method("ANY #{path}", &blk)
+      unbound_method = instance_method "ANY #{path}"
+      passing = proc { unbound_method.bind(self).call; pass }
       %w(get post put delete head).each do |meth|
         send meth, path, *args, &passing
       end
